550be85a1e8edb7fa3dd36a61c8854ce66571245
Anand27git <anandkumar256k@gmail.com>
2021-06-27 21:37:35 +0530

    readandWrite employee Pay roll to Console

---
+++ test/java/com/bridgelab/employeePayrollservices/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelab.employeePayrollservices;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * welcome to Employee pay roll
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
